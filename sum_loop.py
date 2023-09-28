8ad12866c970fb313385a5dd2a7eff97229be992
kobebaffour <kobe.baffour@gmail.com>
2023-09-28 22:04:40 +0000

    Sum Loop

---
+++ sum_loop.py
@@ -0,0 +1,11 @@
+val = 0
+print("Before", val)
+for number in [9,41,12,3,74,15]:
+    val = val + number
+    print(val,number)
+print("The sum of all entries is: ",val)
+
+# Finding the average
+# Sum if all the enteries in array / no. of entries in array
+
+print("The average of all the numbers is: ", val / number)
