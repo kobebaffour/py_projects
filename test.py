439f408121307dca5357851d9d3c826625831e70
kobebaffour <kobe.baffour@gmail.com>
2023-10-31 01:10:38 +0000

    practice

--- test.py
+++ test.py
@@ -1,2 +1,6 @@
-x = "hello"
-print(x)
+def find_max(nums):
+    max_num = float("-inf") # smaller than all other numbers
+    for num in nums:
+        if num > max_num:
+            max_num = num
+return max_num
